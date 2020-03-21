c36c3bf14cf326c40f9b5d1fe4dbb770f2705edc
metamatexbot <metamate+metamatexbot@metamate.com>
2020-03-21 10:12:22 +0100

    Brew formula update for metactl version v0.0.35

--- metactl.rb
+++ metactl.rb
@@ -2,24 +2,24 @@
 class Metactl < Formula
   desc ""
   homepage "https://metamate.io"
-  version "0.0.34"
+  version "0.0.35"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/metamatex/metamate/releases/download/v0.0.34/metactl_0.0.34_darwin_amd64.tar.gz"
-    sha256 "bd6a635a23a4cedefb9f36d5868ce7f1132e7c2bc0f975d27ea1f39a06f27dea"
+    url "https://github.com/metamatex/metamate/releases/download/v0.0.35/metactl_0.0.35_darwin_amd64.tar.gz"
+    sha256 "5b30219eff6ba3fe067a08c74c4776f0fffd4925017c4d2551c08eea190171d9"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/metamatex/metamate/releases/download/v0.0.34/metactl_0.0.34_linux_amd64.tar.gz"
-      sha256 "be8531c7ae9b294a6a69d6b241687b4e3e594c3676cd553e82ce29d19e83da85"
+      url "https://github.com/metamatex/metamate/releases/download/v0.0.35/metactl_0.0.35_linux_amd64.tar.gz"
+      sha256 "01e877a8c227326fc939ee24a0ab20faf938910922bb90064ed4de5a0ff1e8f0"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/metamatex/metamate/releases/download/v0.0.34/metactl_0.0.34_linux_arm64.tar.gz"
-        sha256 "e873c208260a0e62acc29caa141eee1e61a6f8ed074fa539385d281419fa196d"
+        url "https://github.com/metamatex/metamate/releases/download/v0.0.35/metactl_0.0.35_linux_arm64.tar.gz"
+        sha256 "0fc17b440462303267a90b9ce874010541ddd860bd1cfc1365f36c54fbd70603"
       else
-        url "https://github.com/metamatex/metamate/releases/download/v0.0.34/metactl_0.0.34_linux_armv6.tar.gz"
-        sha256 "c0a80cfa32bf796f839b5ffad10e6e6652e77dd90aca46759fa699825325f0b6"
+        url "https://github.com/metamatex/metamate/releases/download/v0.0.35/metactl_0.0.35_linux_armv6.tar.gz"
+        sha256 "ebbc7157a5fea41e95df378a7b3d542adb854addb89f544fa0a4a2aad323bc14"
       end
     end
   end
